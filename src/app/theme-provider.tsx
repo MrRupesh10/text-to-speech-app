2bae4c1b6a5b170feba3274cf92cc0032020e057
Rupesh10 <killerdream11official@gmail.com>
2025-08-02 08:31:54 +0530

    Fix: Corrected next-themes import path

--- src/app/theme-provider.tsx
+++ src/app/theme-provider.tsx
@@ -2,7 +2,8 @@
 
 import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import type { ThemeProviderProps } from 'next-themes/dist/types';
+import type { ThemeProviderProps } from 'next-themes';
+
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider attribute="class" defaultTheme="dark"{...props}>{children}</NextThemesProvider>;
